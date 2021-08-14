16586075f429741cb24561e08742e7285d236ac5
小八 <86550098+xiaojb8@users.noreply.github.com>
2021-08-14 22:36:25 +0800

    修复了重置密码导致的无法登陆问题
    请更新core/core.php

--- core/core.php
+++ core/core.php
@@ -1,6 +1,7 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
 require $_SERVER['DOCUMENT_ROOT'].'/core/DB.php';
+require $_SERVER['DOCUMENT_ROOT'].'/core/config.php';
 $headers = getallheaders();
 if(!isset($headers['X-Token'])){
     $user_token = null;
